3f4d89445fbcf2a85fc02b92da1601e63d0d8fd0
John Paul Cas <casjp.app@gmail.com>
2021-01-29 20:44:48 +0800

    📝 add brief description of functions

--- app/src/main/java/com/fitchstation/android/base/BaseFragment.kt
+++ app/src/main/java/com/fitchstation/android/base/BaseFragment.kt
@@ -12,10 +12,20 @@ abstract class BaseFragment(layoutResourceId: Int): Fragment(layoutResourceId) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         onBindViewResource(view)
+
+        init()
+
     }
 
+    /**
+     * Bind view resource
+     */
     abstract fun onBindViewResource(view: View)
 
+    /**
+     * Function to handle the initialization
+     * of observer/view/components
+     */
     abstract fun init()
 
 }
