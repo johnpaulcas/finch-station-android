93bd725b6c4e1941023f7bb8c0c392edcc31ac82
John Paul Cas <casjp.app@gmail.com>
2021-01-29 21:55:47 +0800

    ♻️ refactor xml files and change FragmentFitchStationBinding to FragmentFinchStationBinding

--- app/src/main/java/com/finchstation/android/ui/finchstation/FinchStationFragment.kt
+++ app/src/main/java/com/finchstation/android/ui/finchstation/FinchStationFragment.kt
@@ -3,17 +3,17 @@ package com.finchstation.android.ui.finchstation
 import android.view.View
 import com.finchstation.android.R
 import com.finchstation.android.base.BaseFragment
-import com.finchstation.android.databinding.FragmentFitchStatonBinding
+import com.finchstation.android.databinding.FragmentFinchStatonBinding
 
 /**
  * Created by johnpaulcas on 29/01/2021.
  */
-class FinchStationFragment: BaseFragment(R.layout.fragment_fitch_staton) {
+class FinchStationFragment: BaseFragment(R.layout.fragment_finch_staton) {
 
-    private var fitchStationBinding: FragmentFitchStatonBinding? = null
+    private var fitchStationBinding: FragmentFinchStatonBinding? = null
 
     override fun onBindViewResource(view: View) {
-        fitchStationBinding = FragmentFitchStatonBinding.bind(view)
+        fitchStationBinding = FragmentFinchStatonBinding.bind(view)
     }
 
     override fun init() {
