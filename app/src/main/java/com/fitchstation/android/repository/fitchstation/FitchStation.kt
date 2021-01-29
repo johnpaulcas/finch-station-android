6c0a4975701918374dae645d19a948a886fa32b1
John Paul Cas <casjp.app@gmail.com>
2021-01-29 21:24:07 +0800

    ✨ add FitchStation base repository

---
+++ app/src/main/java/com/fitchstation/android/repository/fitchstation/FitchStation.kt
@@ -0,0 +1,9 @@
+package com.fitchstation.android.repository.fitchstation
+
+/**
+ * @author johnpaulcas
+ * @since 29/01/2021
+ *
+ */
+interface FitchStation {
+}
---
+++ app/src/main/java/com/fitchstation/android/repository/fitchstation/FitchStationImpl.kt
@@ -0,0 +1,13 @@
+package com.fitchstation.android.repository.fitchstation
+
+/**
+ * @author johnpaulcas
+ * @since 29/01/2021
+ *
+ * Repository to handle the api request and save data to database
+ * return the result to ViewModel
+ *
+ * @see FitchStation
+ */
+class FitchStationImpl: FitchStation {
+}
