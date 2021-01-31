d41b93839e7be9a0e33bf508785b028292c1b34a
John Paul Cas <casjp.app@gmail.com>
2021-01-31 14:35:36 +0800

    ✨ add nav components

---
+++ app/src/main/java/com/finchstation/android/mapper/FinchStationExtensionMapper.kt
@@ -0,0 +1,12 @@
+package com.finchstation.android.mapper
+
+/**
+ * @author johnpaulcas
+ * @since 31/01/2021
+ *
+ * <p>
+ *     This class serve as FinchStation `Extension` class mapper from
+ *     api/finchstation/response to db/entities
+ * </p>
+ */
+
--- app/src/main/java/com/finchstation/android/ui/MainActivity.kt
+++ app/src/main/java/com/finchstation/android/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.finchstation.android.ui
 
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.finchstation.android.R
 import com.finchstation.android.base.BaseActivity
 import com.finchstation.android.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onBindViewResource(): View? {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,6 +20,21 @@ class MainActivity : BaseActivity() {
     }
 
     override fun init() {
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
+//        navController.addOnDestinationChangedListener { _, destination, _ -> {
+//
+//            }
+//        }
+
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
