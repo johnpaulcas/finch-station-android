3f3966f9ccdd1bfaa353481613a0daf3dec5f92d
John Paul Cas <casjp.app@gmail.com>
2021-01-31 14:31:54 +0800

    ✨ add repository to call api base on arch component structure

--- app/src/main/java/com/finchstation/android/repository/finchstation/FinchStationRepository.kt
+++ app/src/main/java/com/finchstation/android/repository/finchstation/FinchStationRepository.kt
@@ -1,9 +1,20 @@
 package com.finchstation.android.repository.finchstation
 
+import androidx.lifecycle.LiveData
+import com.finchstation.android.db.entities.FinchStation
+import com.finchstation.android.helpers.Resource
+
 /**
  * @author johnpaulcas
  * @since 29/01/2021
  *
  */
 interface FinchStationRepository {
+
+    /**
+     * This will handle the fetching of data from api
+     * to the caching to local databae which is room
+     */
+    fun loadFinchStation(): LiveData<Resource<FinchStation>>
+
 }
--- app/src/main/java/com/finchstation/android/repository/finchstation/FinchStationRepositoryImpl.kt
+++ app/src/main/java/com/finchstation/android/repository/finchstation/FinchStationRepositoryImpl.kt
@@ -1,5 +1,16 @@
 package com.finchstation.android.repository.finchstation
 
+import androidx.lifecycle.LiveData
+import com.finchstation.android.AppExecutors
+import com.finchstation.android.api.ApiResponse
+import com.finchstation.android.api.finchstation.FinchStationService
+import com.finchstation.android.api.finchstation.response.FinchStationResponse
+import com.finchstation.android.db.dao.FinchStationDao
+import com.finchstation.android.db.entities.FinchStation
+import com.finchstation.android.helpers.NetworkBoundResource
+import com.finchstation.android.helpers.Resource
+import timber.log.Timber
+
 /**
  * @author johnpaulcas
  * @since 29/01/2021
@@ -9,5 +20,34 @@ package com.finchstation.android.repository.finchstation
  *
  * @see FinchStationRepository
  */
-class FinchStationRepositoryImpl: FinchStationRepository {
+class FinchStationRepositoryImpl(
+        private val appExecutors: AppExecutors,
+        private val finchStationService: FinchStationService,
+        private val finchStationDao: FinchStationDao
+): FinchStationRepository {
+
+    override fun loadFinchStation(): LiveData<Resource<FinchStation>> {
+        return object : NetworkBoundResource<FinchStation, FinchStationResponse>(appExecutors) {
+
+            override fun saveCallResult(item: FinchStationResponse) {
+                Timber.d("Item retrieved $item")
+            }
+
+            override fun shouldFetch(data: FinchStation?): Boolean {
+                return data == null
+            }
+
+            override fun loadFromDb(): LiveData<FinchStation> {
+                Timber.d("Load data from database")
+                return finchStationDao.getAll("Finch Station")
+            }
+
+            override fun createCall(): LiveData<ApiResponse<FinchStationResponse>> {
+                Timber.d("Requesting the data from api")
+                return finchStationService.getFinchStation()
+            }
+
+        }.asLiveData()
+    }
+
 }
