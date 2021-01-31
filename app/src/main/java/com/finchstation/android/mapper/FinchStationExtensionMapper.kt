d250b83dacdff308b9e355387312a2c9d2780a33
John Paul Cas <casjp.app@gmail.com>
2021-01-31 23:08:05 +0800

    ✨ add mapper from api response to db entity

--- app/src/main/java/com/finchstation/android/mapper/FinchStationExtensionMapper.kt
+++ app/src/main/java/com/finchstation/android/mapper/FinchStationExtensionMapper.kt
@@ -1,5 +1,13 @@
 package com.finchstation.android.mapper
 
+import com.finchstation.android.api.finchstation.response.FinchStationResponse
+import com.finchstation.android.api.finchstation.response.models.FinchStationStop
+import com.finchstation.android.api.finchstation.response.models.Route
+import com.finchstation.android.api.finchstation.response.models.RouteStopTime
+import com.finchstation.android.db.entities.FinchStation
+import com.finchstation.android.db.entities.FinchStationRoute
+import com.finchstation.android.db.entities.FinchStationRouteStopTime
+
 /**
  * @author johnpaulcas
  * @since 31/01/2021
@@ -10,3 +18,39 @@ package com.finchstation.android.mapper
  * </p>
  */
 
+fun FinchStationResponse.transformToEntity(): FinchStation {
+    return FinchStation(
+            name,
+            time,
+            uri
+    )
+}
+
+fun FinchStationStop.transferToEntity(
+        foreignKey: String
+): com.finchstation.android.db.entities.FinchStationStop {
+    return com.finchstation.android.db.entities.FinchStationStop(
+            finchStationKey = foreignKey,
+            this.name,
+            this.uri,
+            this.agency
+    )
+}
+
+fun Route.transferToEntity(foreignKey: String): FinchStationRoute {
+    return FinchStationRoute(
+            finchStationKey = foreignKey,
+            this.name,
+            this.uri
+    )
+}
+
+fun RouteStopTime.transferToEnity(foreignKey: String): FinchStationRouteStopTime {
+    return FinchStationRouteStopTime(
+            finchStationRouteKey = foreignKey,
+            this.serviceId,
+            this.shape,
+            this.departureTime,
+            this.departureTimestamp
+    )
+}
