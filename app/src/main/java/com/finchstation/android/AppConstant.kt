aa470ee16eabb3443312e17b0fce4234adcd24fd
John Paul Cas <casjp.app@gmail.com>
2021-02-01 21:46:35 +0800

    🎨 add AppConstant colors

--- app/src/main/java/com/finchstation/android/AppConstant.kt
+++ app/src/main/java/com/finchstation/android/AppConstant.kt
@@ -8,4 +8,15 @@ object AppConstant {
 
     const val BASE_URL = "https://myttc.ca"
 
+     val COLORS = listOf(
+        R.color.red_500,
+        R.color.purple_500,
+        R.color.depp_purple_500,
+        R.color.indego_500,
+        R.color.blue_500,
+        R.color.tea_500,
+        R.color.green_500,
+        R.color.orange_500,
+    )
+
 }
